511d94767ddcd253ced8163136d3998708804146
GitHub Action <action@github.com>
2025-03-23 10:22:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/xepozz/intellijideaplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/xepozz/intellijideaplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.xepozz.intellijideaplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.xepozz.intellijideaplugin.MyBundle
+import com.github.xepozz.intellijideaplugin.services.MyProjectService
 import javax.swing.JButton
 
 
